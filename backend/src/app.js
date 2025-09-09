7f2c20d46e5c9bcb13e656d065d7497729292ce9
Mark Tut <542279+mark-tutton@users.noreply.github.com>
2025-09-09 03:12:25 +0000

    feat: add posts routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { postsRoutes } from './routes/posts.js'
 
+// Setup
 const app = express()
+app.use(cors()) // TODO: add urls
+app.use(bodyParser.json())
+
+// Routes
+postsRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
---
+++ backend/src/routes/posts.js
@@ -0,0 +1,81 @@
+import {
+  listAllPosts,
+  listPostsByAuthor,
+  listPostsByTag,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../services/posts.js'
+
+export function postsRoutes(app) {
+  // get posts
+  app.get('/api/v1/posts', async (req, res) => {
+    // destructure options from req querys
+    const { sortBy, sortOrder, author, tag } = req.query
+    const options = { sortBy, sortOrder }
+
+    try {
+      if (author && tag) {
+        return res
+          .status(400)
+          .json({ error: 'query by either author or tag, not both' })
+      } else if (author) {
+        return res.json(await listPostsByAuthor(author, options))
+      } else if (tag) {
+        return res.json(await listPostsByTag(tag, options))
+      } else {
+        return res.json(await listAllPosts(options))
+      }
+    } catch (err) {
+      console.error('Error listing posts', err)
+      return res.status(500).end()
+    }
+  })
+
+  // get specific post
+  app.get('/api/v1/posts/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+      const post = await getPostById(id)
+      if (post === null) return res.status(404).end()
+      return res.json(post)
+    } catch (err) {
+      console.error('Error getting post', err)
+      return res.status(500).end()
+    }
+  })
+
+  // create a post
+  app.post('/api/v1/posts', async (req, res) => {
+    try {
+      const post = await createPost(req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('Error creating post', err)
+      return res.status(500).end()
+    }
+  })
+  // update a post
+  app.patch('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const post = await updatePost(req.params.id, req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('Error updating post', err)
+      return res.send(500).end()
+    }
+  })
+
+  // delete a post
+  app.delete('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const { deletedCount } = await deletePost(req.params.id)
+      if (deletedCount === 0) return res.sendStatus(404)
+      return res.status(204).end()
+    } catch (err) {
+      console.error('Error deleting post', err)
+    }
+  })
+}
