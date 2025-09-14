313ef19e0bca22c449d1353d235476b7169bc34d
Mark Tut <542279+mark-tutton@users.noreply.github.com>
2025-09-14 22:47:06 +0000

    feat: add frontend components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { Post } from './components/Post.jsx'
+import { CreatePost } from './components/CreatePost'
+import { PostFilter } from './components/PostFilter'
+import { PostSorting } from './components/PostSorting'
+import { PostList } from './components/PostList'
+
+const posts = [
+  {
+    title: 'Full-stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  {
+    title: 'Hello React!',
+  },
+]
 
 export function App() {
   return (
-    <Post
-      title='Full-Stack React Projects'
-      contents='Test'
-      author='Daniel Bugl'
-    />
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
   )
 }
